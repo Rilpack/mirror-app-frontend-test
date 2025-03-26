4edcad90f1b52b1465c612408489a62b252a740e
Rilpack <crays01234@gmail.com>
2025-03-26 12:43:03 +0700

    made request logic on home page with style. add props for card with style correction

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,23 +1,24 @@
 import styles from './Card.module.scss'
 import likeIcon from '@/assets/svg/like.svg'
 import commentIcon from '@/assets/svg/comment.svg'
+import { Post } from '@/typescript/interfaces'
 
-export const Card = () => {
+export const Card = ({ post }: { post: Post }) => {
   return (
     <div className={styles.card}>
-      <p className={styles.card_title}>Text text text text text text text</p>
+      <p className={styles.card_title}>{post.caption}</p>
       <div className={styles.card_bottom}>
         <div className={styles.card_bottom_user}>
-          <b>Josh Banisher</b>
-          <span>25/09/2025</span>
+          <b>{post.user.username}</b>
+          <span>{post.date}</span>
         </div>
         <div className={styles.card_bottom_stats}>
           <div className={styles.card_bottom_stats_section}>
-            <p>245</p>
+            <p>{post.likes}</p>
             <img src={likeIcon} alt='likeIcon' />
           </div>
           <div className={styles.card_bottom_stats_section}>
-            <p>768</p>
+            <p>{post.comments}</p>
             <img src={commentIcon} alt='commentIcon' />
           </div>
         </div>
--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -1,7 +1,36 @@
+import styles from './HomePage.module.scss'
+import { useEffect } from 'react'
+import { useContextProvider } from '@/app'
+import { Card } from '@/components/Card/Card'
+import { usePosts } from '@/hooks/requests/useGetPosts'
+
 export const HomePage = () => {
+  const { settings } = useContextProvider()
+
+  const columns = settings?.layout?.params?.masonry?.columns || 0
+  const rows = settings?.layout?.params?.masonry?.rows || 0
+
+  const { posts, fetchPosts } = usePosts({ limit: columns * rows });
+
+  const columnsData = Array.from({ length: columns }, (_, colIndex) =>
+    posts.slice(colIndex * rows, colIndex * rows + rows)
+  )
+
+  useEffect(() => {
+    fetchPosts();
+  }, [settings])
+
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className={styles.page}>
+      <div className={styles.page_content}>
+        {columnsData.map((items, colIndex) => (
+          <div key={colIndex} className={styles.column}>
+            {items.map((post, rowIndex) => (
+              <Card post={post} key={rowIndex} />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
